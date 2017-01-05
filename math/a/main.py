b45dca36bbf45ff7deaea4383cb63a134b13630d
scott snyder <scott@growmaster420.com>
2017-01-05 14:47:09 -0800

    Create main.py

---
+++ math/a/main.py
@@ -0,0 +1,36 @@
+import math
+
+#var input
+a = int(input("int a\n"))
+b = int(input("int b\n"))
+c = int(input("int c\n"))
+
+print("\noutput\n")
+
+#addition
+print("addition \"a + b + c\"")
+print(a + b + c)
+
+#multiplication
+print("multiplication \"a * b * c\"")
+print(a * b * c)
+
+#subrtaction
+print("subtraction \"a - b - c\"")
+print(a - b - c)
+
+#division
+print("division \"a / b / c\"")
+print(a / b / c)
+
+#powers
+print("powers \"math.pow(a, b)\"")
+print(math.pow(a, b))
+
+#pi
+print("pi \"print(math.pi)\"")
+print(math.pi)
+
+#order of operations
+print("order of operations\"((a / b) + (c / b))\"")
+print((a / b) + (c / b))
