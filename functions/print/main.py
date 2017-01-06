17e17221885dccf255de6fe8759a110613671f3c
scott snyder <scott@growmaster420.com>
2017-01-05 16:06:50 -0800

    Create main.py

---
+++ functions/print/main.py
@@ -0,0 +1,25 @@
+#raw printing
+print("#raw printing use r before \"\"")
+print("#with r")
+print(r"Hello\n World")
+print("#without r")
+print("Hello\n World")
+
+#add strings
+print('#"hello" + " World"')
+print("hello" + " World")
+
+#var and strings
+print('#var1 = ("world")')
+var1 = ("world")
+print('#print("hello", var1)')
+print("hello", var1)
+
+#string input
+var2 = str(input("type hello\n"))
+print(var2, "world")
+
+'''
+print("")
+print("")
+'''
