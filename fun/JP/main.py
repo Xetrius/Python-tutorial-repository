02dcc5edb0a427b40bd754c37f8df72021ed3afd
scott snyder <scott@growmaster420.com>
2017-01-07 00:01:02 -0800

    Update

--- fun/JP/main.py
+++ fun/JP/main.py
@@ -2,7 +2,15 @@
 import time
 import webbrowser
 
-
+print("Jurassic Park System Secruity Interface")
+print("Version 4.0.5, Alpha E")
+print("Ready...")
+while True:
+    var1 = input(">")
+    if var1 == "access main security grid":
+        break
+    if var1 != "access main security grid":
+        print("access: PERMISSION DENIED")
 a = ("https://www.youtube.com/watch?v=fmz-K2hLwSI")
 webbrowser.open_new(a)
 print("access: PERMISSION DENINED", "...and..." )
