51370fd6b42f527070e0f28a14e224248ca75ff0
scott snyder <scott@growmaster420.com>
2017-01-06 23:48:52 -0800

    Update main.py

--- fun/JP/main.py
+++ fun/JP/main.py
@@ -5,10 +5,8 @@ import webbrowser
 
 a = ("https://www.youtube.com/watch?v=fmz-K2hLwSI")
 webbrowser.open_new(a)
-print("access: PERMISSION DENINED", )
-time.sleep(0.1)
-print("...and...")
-time.sleep(0.1)
+print("access: PERMISSION DENINED", "...and..." )
+time.sleep(1)
 while True:
     print ("YOU DIDN'T SAY THE MAGIC WORD")
     time.sleep(0.1)
