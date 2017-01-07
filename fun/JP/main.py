dd9c6acd7e60c5079df847eae0bcc802bee65808
scott snyder <scott@growmaster420.com>
2017-01-06 23:47:49 -0800

    Update main.py

--- fun/JP/main.py
+++ fun/JP/main.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 import time
 import webbrowser
-a = ("https://www.youtube.com/watch?v=fmz-K2hLwSI")
-#b = ("google-chrome")
-#webbrowser.get(using=b)
-print("access: PERMISSION DENINED",)
-time.sleep(.3)
-print("...and...")
-webbrowser.open_new(a)
-time.sleep(.5)
 
+
+a = ("https://www.youtube.com/watch?v=fmz-K2hLwSI")
+webbrowser.open_new(a)
+print("access: PERMISSION DENINED", )
+time.sleep(0.1)
+print("...and...")
+time.sleep(0.1)
 while True:
     print ("YOU DIDN'T SAY THE MAGIC WORD")
     time.sleep(0.1)
