ca16e6e9b2182fecfeaf0387769acf6972e84aac
scott snyder <scott@growmaster420.com>
2017-01-07 00:13:08 -0800

    Update main.py

--- fun/JP/main.py
+++ fun/JP/main.py
@@ -5,16 +5,20 @@ import webbrowser
 print("Jurassic Park System Secruity Interface")
 print("Version 4.0.5, Alpha E")
 print("Ready...")
+
 while True:
     var1 = input(">")
     if var1 == "access main security grid":
-        break
+        break # exit the while loop
     if var1 != "access main security grid":
         print("access: PERMISSION DENIED")
+
+
 a = ("https://www.youtube.com/watch?v=fmz-K2hLwSI")
 webbrowser.open_new(a)
 print("access: PERMISSION DENINED", "...and..." )
 time.sleep(1)
-while True:
+
+for c in range(0, 200):
     print ("YOU DIDN'T SAY THE MAGIC WORD")
     time.sleep(0.1)
