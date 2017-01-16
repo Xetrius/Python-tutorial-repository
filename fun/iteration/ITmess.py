d33ec273bd59ea5100b1d5c5d97621e54c67cb0a
scott snyder <scott@growmaster420.com>
2017-01-15 18:27:57 -0800

    Create ITmess.py

---
+++ fun/iteration/ITmess.py
@@ -0,0 +1,11 @@
+from time import  sleep
+import espeak
+es = espeak.ESpeak()
+var1 = 0
+mess1 = "this is a test."
+while True:
+    var1 = (var1 + 1)
+    var2 = "iteration " + str(var1)
+    es.say(var2)
+    es.say(mess1)
+    sleep(1)
