6fd97f5841975394e1f1028b3fe76f5d14b6520a
growmaster420 <scott@growmaster420.com>
2017-02-15 10:36:51 -0800

    Update fizzbuzz.py
    added interaction

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,7 @@
-
+print("FizzBuzz")
+var2 = int(input("high number?\n"))
 var = 0
-for x in range(1,100):
+for x in range(1,var2):
     var = var + 1
     if var%3==0 and var%5==0:
         print('FizzBuzz')
